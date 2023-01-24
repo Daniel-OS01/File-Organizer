dd1f07f380e129571b2caab74c9136a9668b5416
Daniel.OS88 <31541221+dragon3252@users.noreply.github.com>
2023-01-24 04:40:35 +0200

    Filer Orgnanizer 1.0

---
+++ File-Organizer.ps1
@@ -0,0 +1,34 @@
+function get-destbytype($ext) {
+    switch ($ext) {
+        { $ext -match '(jpg|jpeg|png|gif|bmp|svg|webp)' } { "Photos" }
+        { $ext -match '(divx|mpeg|avi|mkv|mp4)' } { "Video" }
+        { $ext -match '(js|java|py|class|vbs|jsp)' } { "Code" }
+        { $ext -match '(sql|ps1|pl|py|scpt)' } { "Scripts" }
+        { $ext -match '(zip|7z|rar|tar|bz2|gz)' } { "Compressed" }
+        { $ext -match '(doc|docx|pdf)' } { "Documents" }
+        { $ext -match '(xls|xlsx|csv)' } { "Excel" }
+        { $ext -match '(exe|dll|jar|ear|bat|cmd|msi)' } { "Applications" }
+        { $ext -match '(txt|log)' } { "Text" }
+        { $ext -match '(msg)' } { "Miscellaneous" }
+        { $ext -match '(properties|xml|ini)' } { "Config" }
+        { $ext -match '(m4a|mp3)' } { "Audio" }
+        { $ext -match '(bib|ris|ini)' } { "References" }
+        { $ext -match '(pptx|ppt)' } { "Presentations" }
+        { $ext -match '(ipsw|img|iso|vsix)' } { "ISO-Images" }
+        { $ext -match '(epub|img|iso)' } { "Ebooks" }
+        { $ext -match '(htm|html|lnk|php)' } { "Website Related" }
+        default { "$ext" }
+    }
+}
+# Folder to organize the files in
+$dirtyfolder = "F:\Downloads\"
+$org = $dirtyfolder
+
+ls $dirtyfolder/* | ? { !$_.PSIsContainer } | % {
+    $ext = $_.extension
+    $dest = "$($org)\$(get-destbytype $ext)"
+    if (! (Test-Path -path $dest ) ) {
+        New-Item $dest -type directory
+    }
+    Move-Item -Path $_.FullName -Destination $dest -Force
+}
